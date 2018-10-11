3895ca2171147fb3488b9712d5ccc78156c31207
Ricardo <ricardomessier@gmail.com>
2018-10-11 17:06:57 -0500

    core object

---
+++ src/main/java/com/revature/model/Employee.java
@@ -0,0 +1,9 @@
+package com.revature.model;
+
+public class Employee extends User {
+
+	public Employee(int id, String username, String password, String firstname, String lastname, String email) {
+		super(id, username, password, firstname, lastname, email);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/main/java/com/revature/model/Manager.java
@@ -0,0 +1,9 @@
+package com.revature.model;
+
+public class Manager extends User {
+
+	public Manager(int id, String username, String password, String firstname, String lastname, String email) {
+		super(id, username, password, firstname, lastname, email);
+	}
+
+}
