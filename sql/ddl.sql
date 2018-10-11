0bcad450ef62aed39836323e5ef16be9cadd3545
Ricardo <ricardomessier@gmail.com>
2018-10-11 16:19:13 -0500

    updated tables

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,39 +1,39 @@
 CREATE TABLE ERS_USERS (
-    u_id number primary key,
-    u_username varchar2(40),
-    u_password varchar2(40),
+    u_id number primary key not null,
+    u_username varchar2(40) unique not null,
+    u_password varchar2(40) not null,
     u_firstname varchar2(40),
     u_lastname varchar2(40),
-    u_email varchar2(40),
+    u_email varchar2(40) unique,
     ur_id number
 );
 
 CREATE TABLE ERS_USER_ROLES (
-    ur_id number primary key,
+    ur_id number primary key not null,
     ur_role varchar2(40)
 );
 
 CREATE TABLE ERS_REINBURSEMENTS (
-    r_id number primary key,
-    r_amount number,
+    r_id number primary key not null,
+    r_amount number not null,
     r_description varchar2(100), 
     r_receipt blob,
-    r_submitted timestamp,
+    r_submitted timestamp not null,
     r_resolved timestamp,
-    u_id_author number,
+    u_id_author number not null,
     u_id_resolver number,
-    rt_type number,
-    rs_status number
+    rt_id number not null,
+    rs_id number not null
 );
 
 CREATE TABLE ERS_REIMBURSEMENT_STATUS (
-    rs_id number,
-    rs_status varchar2(30)
+    rs_id number primary key,
+    rs_status varchar2(30) not null
 );
 
 CREATE TABLE ERS_REINBURSEMENT_TYPE (
-    rt_id number,
-    rt_type varchar(30)
+    rt_id number primary key,
+    rt_type varchar(30) not null
 );
 
 ALTER TABLE ERS_USERS ADD CONSTRAINT FKUserRoles
@@ -49,9 +49,9 @@ FOREIGN KEY (u_id_resolver) REFERENCES ERS_USERS (u_id)
 ON DELETE CASCADE;
 
 ALTER TABLE ERS_REINBURSEMENTS ADD CONSTRAINT FKType
-FOREIGN KEY (rt_type) REFERENCES ERS_REINBURSEMENT_TYPE (rt_type)
+FOREIGN KEY (rt_id) REFERENCES ERS_REINBURSEMENT_TYPE (rt_id)
 ON DELETE CASCADE;
 
 ALTER TABLE ERS_REINBURSEMENTS ADD CONSTRAINT FKStatus
-FOREIGN KEY (rs_status) REFERENCES ERS_REIMBURSEMENT_STATUS (rs_status)
+FOREIGN KEY (rs_id) REFERENCES ERS_REIMBURSEMENT_STATUS (rs_id)
 ON DELETE CASCADE;
