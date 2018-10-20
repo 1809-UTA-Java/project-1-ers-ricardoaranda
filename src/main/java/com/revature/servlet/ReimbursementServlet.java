73e426eaff221c7fe24e8bd690e3d62ac7e20dba
Ricardo <ricardomessier@gmail.com>
2018-10-20 15:40:13 -0500

    can now upload receipt images to DB

--- src/main/java/com/revature/servlet/ReimbursementServlet.java
+++ src/main/java/com/revature/servlet/ReimbursementServlet.java
@@ -1,22 +1,26 @@
 package com.revature.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+
+import org.apache.commons.io.IOUtils;
 
 import com.revature.model.Employee;
 import com.revature.model.Reimbursements;
-import com.revature.repository.EmployeeDao;
 import com.revature.repository.ReimbursementDao;
 
+@MultipartConfig
 @WebServlet("/submit-form")
 public class ReimbursementServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -37,10 +41,19 @@ public class ReimbursementServlet extends HttpServlet {
 		
 		String amount = request.getParameter("amount");
 		String description = request.getParameter("description");
+		Part filePart = request.getPart("receipt");
+		byte[] image = null;
+		
+		InputStream inputStream = filePart.getInputStream();
 		
 		Employee employee = (Employee) request.getSession().getAttribute("user");
 		
 		Reimbursements newReimbursement = new Reimbursements(reimbursementId, Integer.parseInt(amount), description, time, employee, "travel", "pending");
+		
+		if (filePart != null) {
+			image = IOUtils.toByteArray(inputStream);
+			newReimbursement.setReceipt(image);
+		}
 		
 		rdao.saveReimbursement(newReimbursement);
 		
